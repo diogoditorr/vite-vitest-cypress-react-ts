ab8924839c527b557c1331a043c2aaa9c3780e57
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2023-06-12 22:22:49 -0300

    refactor: organize app and css files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "@/assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "@/assets/css/App.css";
 
 function App() {
     const [count, setCount] = useState(0);
+
     return (
         <>
             <div>
@@ -19,7 +20,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1 className="title">Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
